8bfe8b7db60ba77d74b6473dcfc5e8486fae88bc
kalid <khalidmeftu@gmail.com>
2025-02-27 12:49:37 +0300

    added auth fixes to google login route

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserPreferenceController;
-use App\Http\Controllers\GoogleLoginController;
+use App\Http\Controllers\Auth\GoogleLoginController;
 
 /*
 |--------------------------------------------------------------------------
